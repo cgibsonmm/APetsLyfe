503aa484e8672b9bbbca4289899758d1406af6c9
Corey Gibson <cgibsonmm@gmail.com>
2017-11-30 18:16:56 -0500

    added annotation to project

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: user_profiles
+#
+#  id                  :integer          not null, primary key
+#  user_id             :integer
+#  about               :text
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#  age                 :integer
+#  city                :string
+#  state               :string
+#  zip_code            :integer
+#
+
 class UserProfile < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
+# == Route Map
+#
+#                   Prefix Verb   URI Pattern                       Controller#Action
+#         new_user_session GET    /users/sign_in(.:format)          devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)          devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)         devise/sessions#destroy
+#        new_user_password GET    /users/password/new(.:format)     devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)    devise/passwords#edit
+#            user_password PATCH  /users/password(.:format)         devise/passwords#update
+#                          PUT    /users/password(.:format)         devise/passwords#update
+#                          POST   /users/password(.:format)         devise/passwords#create
+# cancel_user_registration GET    /users/cancel(.:format)           registrations#cancel
+#    new_user_registration GET    /users/sign_up(.:format)          registrations#new
+#   edit_user_registration GET    /users/edit(.:format)             registrations#edit
+#        user_registration PATCH  /users(.:format)                  registrations#update
+#                          PUT    /users(.:format)                  registrations#update
+#                          DELETE /users(.:format)                  registrations#destroy
+#                          POST   /users(.:format)                  registrations#create
+#            welcome_index GET    /welcome/index(.:format)          welcome#index
+#                     root GET    /                                 home#index
+#            user_profiles GET    /user_profiles(.:format)          user_profiles#index
+#                          POST   /user_profiles(.:format)          user_profiles#create
+#         new_user_profile GET    /user_profiles/new(.:format)      user_profiles#new
+#        edit_user_profile GET    /user_profiles/:id/edit(.:format) user_profiles#edit
+#             user_profile GET    /user_profiles/:id(.:format)      user_profiles#show
+#                          PATCH  /user_profiles/:id(.:format)      user_profiles#update
+#                          PUT    /user_profiles/:id(.:format)      user_profiles#update
+#                          DELETE /user_profiles/:id(.:format)      user_profiles#destroy
+# 
+
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations'}
   get 'welcome/index'
