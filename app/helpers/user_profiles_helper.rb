47122baa1686b59efdd0a81c775eccb664c66f2b
Corey Gibson <cgibsonmm@gmail.com>
2017-12-07 17:30:24 -0500

    working croper with minor changes

--- app/helpers/user_profiles_helper.rb
+++ app/helpers/user_profiles_helper.rb
@@ -4,4 +4,12 @@ module UserProfilesHelper
                 LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND
                 OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY)
   end
+
+  def display_profile_image(profile)
+    if profile.avatar_file_name.nil?
+      image_tag 'blank_img.jpg'
+    else
+      image_tag profile.avatar.url(:thumb), alt: 'where are you', class: 'img-fluid'
+    end
+  end
 end
