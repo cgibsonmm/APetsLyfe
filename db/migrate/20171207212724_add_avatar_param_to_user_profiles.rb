96733cebcd94ddcd55e593a49b37e853afe660c5
Corey Gibson <cgibsonmm@gmail.com>
2017-12-07 17:45:48 -0500

    fixed typo in migration file

--- db/migrate/20171207212724_add_avatar_param_to_user_profiles.rb
+++ db/migrate/20171207212724_add_avatar_param_to_user_profiles.rb
@@ -1,10 +1,10 @@
 class AddAvatarParamToUserProfiles < ActiveRecord::Migration[5.1]
   def change
-    add_column :user_profiles, :avatar_original_w, :interger
-    add_column :user_profiles, :avatar_original_h, :interger
-    add_column :user_profiles, :avatar_crop_x,     :interger
-    add_column :user_profiles, :avatar_crop_y,     :interger
-    add_column :user_profiles, :avatar_crop_w,     :interger
-    add_column :user_profiles, :avatar_crop_h,     :interger
+    add_column :user_profiles, :avatar_original_w, :integer
+    add_column :user_profiles, :avatar_original_h, :integer
+    add_column :user_profiles, :avatar_crop_x,     :integer
+    add_column :user_profiles, :avatar_crop_y,     :integer
+    add_column :user_profiles, :avatar_crop_w,     :integer
+    add_column :user_profiles, :avatar_crop_h,     :integer
   end
 end
