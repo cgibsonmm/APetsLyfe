4be9fbf199d2e5aaba3713bcdf05c6667fc45dba
Corey Gibson <cgibsonmm@gmail.com>
2018-01-22 18:22:33 -0500

    working on image posts

--- app/helpers/image_posts_helper.rb
+++ app/helpers/image_posts_helper.rb
@@ -1,2 +1,10 @@
 module ImagePostsHelper
+
+  def current_pets
+    pets = []
+    current_user.pets.each do |pet|
+      pets << pet
+    end
+    pets
+  end
 end
--- app/models/image_post.rb
+++ app/models/image_post.rb
@@ -22,4 +22,6 @@
 class ImagePost < ApplicationRecord
   belongs_to :user
   belongs_to :pet
+  has_attached_file :image, styles: { medium: "640x640", thumb: "200x200#" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
